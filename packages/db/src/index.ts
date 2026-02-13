b9d7dd93892aca8d40b8c7d491904c2997f8e0f5
Victor-Okenwa <okenwavictor003@gmail.com>
2026-02-13 19:37:12 +0100

    Remove conole log in db

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,5 +2,4 @@ import { env } from "@dealort/utils/env";
 import { db as nodeDb } from "./client/node";
 import { db as serverlessDb } from "./client/serverless";
 
-console.log(env.NODE_ENV);
 export const db = env.NODE_ENV === "production" ? serverlessDb : nodeDb;
